190505d16c12778f0c80620974472f52a1a741d1
ThantWaiYanLinn <twaiyy27@gmail.com>
2026-01-03 15:53:10 +0700

    adjust email info in .env

--- ticketanywhere/settings.py
+++ ticketanywhere/settings.py
@@ -153,10 +153,10 @@ SIMPLE_JWT = {
 
 LOGIN_REDIRECT_URL = '/api/' 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
 
